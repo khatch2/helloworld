948d716210c37115cdb893a11f456e22fd3f2826
khatch2_name <khatchshah2@gmail.com>
2024-08-10 18:15:44 +0200

    Created launch.json

--- helloworld.cpp
+++ helloworld.cpp
@@ -9,9 +9,11 @@ int main(void)
     std::vector<std::string> msg {"Hello", "C++", "world", "from", "VS Code", "and C++ extensions!"};
     // msg.assign()
 
+    int i = 0;
     for (const std::string& word : msg)
     {
         std::cout << word << " ";
+        ++i;
     }
     std::cout << std::endl;
 
